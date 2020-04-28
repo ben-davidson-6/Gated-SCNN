6f1461f4a66ffa671f6d0524466591cbfe301cfd
ben <ben@disperse.io>
2020-04-28 17:06:23 +0100

    MODEL REGRESSION COMMIT

--- cityscapes_model.py
+++ cityscapes_model.py
@@ -9,12 +9,12 @@ from gscnn.model_definition import GSCNN
 from gscnn.train_and_evaluate import Trainer
 
 
-batch_size = 8
+batch_size = 7
 network_input_h = network_input_w = 513
 max_crop_downsample = 0.5
 colour_aug_factor = 0.2
 l1 = 1.
-l2 = 10.
+l2 = 1.
 l3 = 1.
 l4 = 1.
 loss_weights = [l1, l2, l3, l4]
@@ -33,7 +33,7 @@ cityscapes_dataset_loader = cityscapes.dataset.CityScapes(
 model = GSCNN(n_classes=cityscapes.N_CLASSES)
 # momentum = 0.9
 # learning_rate_fn = tf.keras.optimizers.schedules.PolynomialDecay(
-#     7e-2,
+#     1e-4,
 #     n_steps_in_epoch * 230,
 #     0.000001)
 # optimiser = tf.keras.optimizers.SGD(learning_rate=learning_rate_fn, momentum=momentum)
