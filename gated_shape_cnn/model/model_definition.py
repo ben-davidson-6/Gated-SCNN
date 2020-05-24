moved gradient function to single place, and fixed a bug with empty images, also upddate the validation to use tf.debugging instead of assert

gated_shape_cnn/model/model_definition.py
@@ -2,7 +2,7 @@ import imageio
 import tensorflow as tf
 
 from gated_shape_cnn.model.layers import (
-    ShapeStream, AtrousPyramidPooling, FinalLogitLayer, XceptionBackbone)
+    gradient_mag, ShapeStream, AtrousPyramidPooling, FinalLogitLayer, XceptionBackbone)
 
 
 class GSCNN(tf.keras.Model):
@@ -14,14 +14,6 @@ class GSCNN(tf.keras.Model):
         self.shape_stream = ShapeStream()
         self.atrous_pooling = AtrousPyramidPooling(out_channels=256)
         self.logit_layer = FinalLogitLayer(self.n_classes)
-
-    def sobel_edges(self, tensor, eps=1e-12):
-        gray = tf.image.rgb_to_grayscale(tensor[..., :3])
-        tensor_edge = tf.image.sobel_edges(gray)
-        mag = tf.reduce_sum(tensor_edge ** 2, axis=-1) + eps
-        mag = tf.math.sqrt(mag)
-        mag /= tf.reduce_max(mag, axis=[1, 2], keepdims=True)
-        return mag
 
     def call(self, inputs, training=None, mask=None):
         # Backbone
@@ -35,7 +27,7 @@ class GSCNN(tf.keras.Model):
         backbone_features = [s1, s2, s3, s4]
 
         # edge stream
-        edge = self.sobel_edges(inputs)
+        edge = gradient_mag(inputs, from_rgb=True)
         shape_activations, edge_out = self.shape_stream(
             [backbone_features, edge],
             training=training)

gated_shape_cnn/training/loss.py
@@ -1,7 +1,8 @@
 import tensorflow as tf
+from gated_shape_cnn.model.layers import gradient_mag
 
 
-def _generalised_dice(y_true, y_pred, eps=0.0):
+def _generalised_dice(y_true, y_pred, eps=0.0, from_logits=True):
     """
     :param y_true [b, h, w, c]:
     :param y_pred [b, h, w, c]:
@@ -10,8 +11,10 @@ def _generalised_dice(y_true, y_pred, eps=0.0):
 
     see https://www.nature.com/articles/s41598-018-26350-3
     """
+
     # [b, h, w, classes]
-    y_pred = tf.nn.softmax(y_pred)
+    if from_logits:
+        y_pred = tf.nn.softmax(y_pred)
     y_true_shape = tf.shape(y_true)
 
     # [b, h*w, classes]
@@ -32,19 +35,6 @@ def _generalised_dice(y_true, y_pred, eps=0.0):
     dices = 1. - 2. * numerators / denom
     dices = tf.where(tf.math.is_finite(dices), dices, tf.zeros_like(dices))
     return tf.reduce_mean(dices)
-
-
-def _edge_mag(tensor, eps=1e-8):
-    """
-    - get image gradient magnitudes and normalise by largest mag
-    - for an n channel image you get dx,dy for all n channels, so will
-      have n magnitudes
-    """
-    tensor_edge = tf.image.sobel_edges(tensor)
-    mag = tf.reduce_sum(tensor_edge ** 2, axis=-1) + eps
-    mag = tf.math.sqrt(mag)
-    mag /= tf.reduce_max(mag, axis=[1, 2], keepdims=True)
-    return mag
 
 
 def _gumbel_softmax(logits, eps=1e-8, tau=1.):
@@ -77,11 +67,11 @@ def _segmentation_edge_loss(gt_tensor, logit_tensor, thresh=0.8):
     logit_tensor = _gumbel_softmax(logit_tensor)
 
     # normalised image gradients to give us edges
-    # images will be [b, h, w, 1]
-    gt_edges = _edge_mag(gt_tensor)
-    pred_edges = _edge_mag(logit_tensor)
+    # images will be [b, h, w, n_classes]
+    gt_edges = gradient_mag(gt_tensor)
+    pred_edges = gradient_mag(logit_tensor)
 
-    # [b, h*W]
+    # [b*h*w, n]
     gt_edges = tf.reshape(gt_edges, [-1, tf.shape(gt_edges)[-1]])
     pred_edges = tf.reshape(pred_edges, [-1, tf.shape(gt_edges)[-1]])
 
@@ -160,6 +150,10 @@ def _weighted_cross_entropy(y_true, y_pred, keep_mask):
 
 
 def loss(gt_label, logits, shape_head, edge_label, loss_weights):
+    # tf.debugging.assert_rank(y_true, 4, message='y_true')
+    # tf.debugging.assert_rank(y_pred, 4, message='y_true')
+    # tf.debugging.assert_rank(y_pred, 4, message='y_true')
+    # tf.debugging.assert_rank(y_pred, 4, message='y_true')
 
     # in cityscapes we ignore some classes, which means that there will
     # be pixels without any class

gated_shape_cnn/training/utils.py
@@ -3,58 +3,65 @@ import tensorflow as tf
 from scipy.ndimage import distance_transform_edt
 
 
-def validate_tensor_rank(tensor, name):
-    rank = tf.rank(tensor)
-    tf.assert_equal(
-        rank,
-        4,
-        message='{} should be rank 4, saw: {}'.format(name, rank))
-
-
 def validate_edge_tensor(edge):
-    validate_tensor_rank(edge, 'edges')
-    edge_channels = tf.shape(edge)[-1]
-    edge_channel_str = 'edges should have 2 channels, saw: {}'.format(edge_channels)
-    tf.assert_equal(
-        edge_channels,
-        2,
-        message=edge_channel_str)
+    tf.debugging.assert_shapes(
+        [(edge, ('b', 'h', 'w', 2))],
+        message='edges')
+    tf.debugging.assert_type(
+        edge,
+        tf.float32,
+        message='edges')
 
 
 def validate_label_tensor(label):
-    validate_tensor_rank(label, 'labels')
-    label_channels = tf.shape(label)[-1]
-    label_channel_str = 'labels should have at least 2 channels, saw {}'.format(label_channels)
-    tf.assert_greater(
-        label_channels,
-        1,
-        message=label_channel_str)
+    tf.debugging.assert_rank(
+       label,
+        4,
+        message='label')
+    # raise value error for consistency with other validations
+    try:
+        tf.debugging.assert_greater_equal(
+            tf.shape(label)[-1],
+            2)
+    except tf.errors.InvalidArgumentError:
+        raise ValueError('must have at least 2 channels in label')
+    tf.debugging.assert_type(
+        label,
+        tf.float32,
+        message='label')
 
 
 def validate_image_tensor(image):
-    validate_tensor_rank(image, 'images')
-    image_channels = tf.shape(image)[-1]
-    image_channel_str = 'images should have at least 3 channels, saw {}'.format(image_channels)
-    tf.assert_equal(
-        image_channels,
-        3,
-        message=image_channel_str)
+    tf.debugging.assert_shapes(
+        [(image, ('b', 'h', 'w', 3))],
+        message='image'
+    )
+    tf.debugging.assert_type(
+        image,
+        tf.float32,
+        message='image')
 
 
-def _label_to_one_hot(label, n_classes):
+def _label_to_one_hot_for_boundary(label, n_classes, background_class):
     """
         Converts a segmentation mask (H,W) to (H,W,K) where the last dim is a one
         hot encoding vector
         """
-    _mask = [label == (i + 1) for i in range(n_classes)]
+    assert label.ndim == 2, 'label must be of shape (h, w)'
+    _mask = []
+    for i in range(n_classes):
+        if i != background_class:
+            _mask.append(label == i)
+        else:
+            _mask.append(np.zeros_like(label, dtype=np.bool))
     return np.stack(_mask, axis=-1).astype(np.uint8)
 
 
-def flat_label_to_edge_label(label, n_classes, radius=2):
+def flat_label_to_edge_label(label, n_classes, radius=2, background_class=0):
     """
-    Converts a segmentation label (H,W) to a binary edgemap (H,W)
+    Converts a segmentation label (H,W) to a binary edgemap (H,W, 1)
     """
-    one_hot = _label_to_one_hot(label, n_classes)
+    one_hot = _label_to_one_hot_for_boundary(label, n_classes, background_class)
     one_hot_pad = np.pad(one_hot, ((1, 1), (1, 1), (0, 0)), mode='constant', constant_values=0)
     edgemap = np.zeros(one_hot.shape[:-1])
 

tests/unit/model/layers.py
@@ -45,6 +45,54 @@ class TestResize(unittest.TestCase):
         self.assertEqual(out_t.get_shape()[1:-1], target_t.get_shape()[1:-1])
 
 
+class TestGradMag(tf.test.TestCase):
+
+    def test_correct_gradients(self):
+        tensor = tf.constant([
+            [0., 0, 1, 0., 0.],
+            [0., 0, 1, 0., 0.],
+        ])[None, :, :, None]
+        out = gscnn_layers.gradient_mag(tensor)
+        should_out = tf.constant([
+            [0., 1, 0, 1., 0.],
+            [0., 1, 0, 1., 0.],
+        ])[None, :, :, None]
+        self.assertEqual(out.get_shape(), tf.TensorShape([1, 2, 5, 1]))
+        self.assertAllClose(out, should_out)
+
+        tensor = tf.constant([
+            [0., 0.],
+            [0., 0.],
+            [1., 1.],
+            [0., 0.],
+            [0., 0.],
+        ])[None, :, :, None]
+        out = gscnn_layers.gradient_mag(tensor)
+        should_out = tf.constant([
+            [0., 0.],
+            [1., 1.],
+            [0., 0.],
+            [1., 1.],
+            [0., 0.],
+        ])[None, :, :, None]
+        self.assertEqual(out.get_shape(), tf.TensorShape([1, 5, 2, 1]))
+        self.assertAllClose(out, should_out)
+
+    def test_empty_image(self):
+        tensor = tf.constant([
+            [0., 0, 0, 0., 0.],
+            [0., 0, 0, 0., 0.],
+        ])[None, :, :, None]
+        out = gscnn_layers.gradient_mag(tensor)
+        should_out = tf.constant([
+            [0., 0, 0, 0., 0.],
+            [0., 0, 0, 0., 0.],
+        ])[None, :, :, None]
+        self.assertEqual(out.get_shape(), tf.TensorShape([1, 2, 5, 1]))
+        print(out)
+        self.assertAllClose(out, should_out)
+
+
 class TestGateConv(tf.test.TestCase):
     def setUp(self):
         self.tensor = tf.random.uniform([1, 16, 16, 3])
@@ -255,12 +303,11 @@ class TestXceptionBackbone(tf.test.TestCase):
         out = self.xception(self.tensor)
         self.assertEqual(len(out), 4)
         self.assertEqual(type(out), dict)
-        # self.assertEqual(out.get_shape(), self.tensor_shape[:-1] + (self.num_classes,))
 
-    # def test_batch_norm_doing_something(self):
-    #     y_training = self.logit_layer(self.tensor, training=True)
-    #     y_inference = self.logit_layer(self.tensor, training=False)
-    #     self.assertNotAllClose(y_inference, y_training)
+    def test_batch_norm_doing_something(self):
+        y_training = self.logit_layer(self.tensor, training=True)
+        y_inference = self.logit_layer(self.tensor, training=False)
+        self.assertNotAllClose(y_inference, y_training)
 
 
 if __name__ == '__main__':